07fece92ad54154a12397c952de8a01cc4856d2b
Teo_supperman <mongtritai.se@gmail.com>
2021-04-08 15:11:13 +0700

    First update

---
+++ src/main/java/com/company/SpringbootWebBanHangApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebBanHangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebBanHangApplication.class, args);
+	}
+
+}
